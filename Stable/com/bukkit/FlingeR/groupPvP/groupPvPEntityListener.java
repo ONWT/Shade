a275cd7114200487afc6aef2fe935b2b0f329e2f
FlingeR <flinger2@gmail.com>
2011-04-09 14:12:29 -0300

    Releasing 0.4:  - Fixed for CB-670.

--- Stable/com/bukkit/FlingeR/groupPvP/groupPvPEntityListener.java
+++ Stable/com/bukkit/FlingeR/groupPvP/groupPvPEntityListener.java
@@ -15,30 +15,35 @@ public class groupPvPEntityListener extends EntityListener {
 			if ( (entEvent.getDamager() instanceof Player) && (entEvent.getEntity() instanceof Player) ) {
 				Player attacker = (Player)entEvent.getDamager();
 				Player defender = (Player)entEvent.getEntity();
+				String world = attacker.getWorld().toString();
+				world = world.replace("CraftWorld{name=", "");
+				world = world.replace("}", "");
 	    		if (groupPvP.hasPermissions(attacker, "groupPvP.denyattack")) {
 				event.setCancelled(true);
 				String Attacker=null;
+				String DG=groupPvP.Permissions.getGroup(world,defender.getName());
 	    		Attacker = groupPvP.pconfig.get("deny-attack").replace("%d", defender.getName());
-	    		Attacker = Attacker.replace("%g", groupPvP.Permissions.getGroup(defender.getName()));
+	    		Attacker = Attacker.replace("%g", DG);
 				attacker.sendMessage(Attacker);
 	    		}
 				else{
 		    		if(groupPvP.hasPermissions(defender, "groupPvP.protected")){
 						event.setCancelled(true);
 						String Attacker=null;
+						String DG=groupPvP.Permissions.getGroup(world,defender.getName());
 			    		Attacker = groupPvP.pconfig.get("deny-protected").replace("%d", defender.getName());
-			    		Attacker = Attacker.replace("%g", groupPvP.Permissions.getGroup(defender.getName()));
+			    		Attacker = Attacker.replace("%g", DG);
 						attacker.sendMessage(Attacker);
 		    		}
 						else
 						{
-							String AG=groupPvP.Permissions.getGroup(attacker.getName());
-							String DG=groupPvP.Permissions.getGroup(defender.getName());
+							String AG=groupPvP.Permissions.getGroup(world,attacker.getName());
+							String DG=groupPvP.Permissions.getGroup(world,defender.getName());
 				    		if(groupPvP.hasPermissions(attacker, "groupPvP.blockgroup")&&AG.equals(DG)){
 								event.setCancelled(true);
 								String Attacker=null;
 					    		Attacker = groupPvP.pconfig.get("deny-own-group").replace("%d", defender.getName());
-					    		Attacker = Attacker.replace("%g", AG);								
+					    		Attacker = Attacker.replace("%g", AG.trim());								
 								attacker.sendMessage(Attacker);
 						}
 		    		}
